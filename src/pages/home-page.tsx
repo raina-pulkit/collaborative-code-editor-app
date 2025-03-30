f97740d172a38967c609a3175b3787fe23414679
raina-pulkit <pulkitraina2608@gmail.com>
2025-03-30 23:15:49 +0530

    Update button text for room creation and enhance layout in HomePage component
    - Change button text from 'Create Random Room' to 'Create New Room'
    - Refactor layout to improve spacing and organization of the room ID input and buttons

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,15 +1,15 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { CREATE_ROOM } from '@/constants/button-texts';
 import { ROUTES } from '@/constants/routes';
 import { useUser } from '@/context/user-context';
 import { Header } from '@/custom/header';
 import { Box, Container, Typography } from '@mui/material';
 import { JSX, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { LoadingPage } from './loading-page';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import { v7 as uuid, validate } from 'uuid';
 import { toast } from 'sonner';
-import { CREATE_ROOM } from '@/constants/button-texts';
+import { v7 as uuid, validate } from 'uuid';
+import { LoadingPage } from './loading-page';
 
 const HomePage = (): JSX.Element => {
   const { userDetails, isLoading, error } = useUser();
@@ -40,6 +40,7 @@ const HomePage = (): JSX.Element => {
           textAlign: 'center',
           padding: '2rem',
           color: 'white',
+          gap: '2rem',
         }}
       >
         <Typography variant="h2" component="h1" gutterBottom>
@@ -52,36 +53,44 @@ const HomePage = (): JSX.Element => {
           Start coding with your team in real-time. Create or join a session to
           begin collaborating.
         </Typography>
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            if (validate(roomId)) navigate(`${ROUTES.EDITOR}/${roomId}`);
-            else
-              toast.error('Invalid room id', {
-                style: {
-                  backgroundColor: 'red',
-                  color: 'white',
-                },
-              });
-          }}
-          className="flex gap-2 mt-10"
-        >
-          <Input
-            placeholder="Enter room id"
-            value={roomId}
-            onChange={e => setRoomId(e.target.value)}
-          />
-          <Button type="submit">Join Room</Button>
-        </form>
-        <Button
-          variant="outline"
-          onClick={() => {
-            const roomId = uuid();
-            navigate(`${ROUTES.EDITOR}/${roomId}`);
-          }}
-        >
-          {CREATE_ROOM}
-        </Button>
+        <div className="flex gap-4 min-w-2xl">
+          <Button
+            variant="outline"
+            onClick={() => {
+              const roomId = uuid();
+              navigate(`${ROUTES.EDITOR}/${roomId}`);
+            }}
+            className="text-black hover:bg-accent-foreground hover:text-white active:scale-95 transition-all duration-300 cursor-pointer"
+          >
+            {CREATE_ROOM}
+          </Button>
+          <form
+            onSubmit={e => {
+              e.preventDefault();
+              if (validate(roomId)) navigate(`${ROUTES.EDITOR}/${roomId}`);
+              else
+                toast.error('Invalid room id', {
+                  style: {
+                    backgroundColor: 'red',
+                    color: 'white',
+                  },
+                });
+            }}
+            className="flex w-full gap-2"
+          >
+            <Input
+              placeholder="Enter room id"
+              value={roomId}
+              onChange={e => setRoomId(e.target.value)}
+            />
+            <Button
+              type="submit"
+              className="cursor-pointer hover:bg-white hover:text-black active:scale-95 transition-all duration-300"
+            >
+              Join Room
+            </Button>
+          </form>
+        </div>
       </Box>
     </Container>
   );
